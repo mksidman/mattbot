73c71d50962b62a7eadcd7ef7d0184147792b662
mksidman <mksidman@gmail.com>
2016-03-30 17:22:41 -0700

    Update math.js

--- scripts/math.js
+++ scripts/math.js
@@ -1,15 +1,15 @@
 module.exports = function(robot) {
   robot.respond(/calculate (.*) of (.*)/i, function(res) {
     var calc = res.match[1];
-    var stringArray = res.match[2].split(',');
+    var numArray = res.match[2].split(',').map(Number);
 
     //WIP on verification of Array object and numbers
-    if (stringArray instanceof Array) {
-        numArray = stringArray.map(Number);
-    } else {
-      res.send("Please enter a comma-separated list of numbers (ex. 1,2,3,4)");
-      break;
-    }
+    // if (stringArray instanceof Array) {
+    //     numArray = stringArray.map(Number);
+    // } else {
+    //   res.send("Please enter a comma-separated list of numbers (ex. 1,2,3,4)");
+    //   break;
+    // }
 
     //calculates mean or median based on array input 
     switch (calc) {
